0d5591a761eece759d630a944ad70d182bd65011
joearrowsmith <joearrowsmith98@gmail.com>
2021-05-11 09:48:32 +0100

    adding sample tod

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
-from flask import Flask, render_template, url_for, request
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
-  app = Flask(__name__)
-  Bootstrap(app)
-  return app
+    app = Flask(__name__)
+    return app
 
 app = create_app()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    visible = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Todo {self.id}-{self.title}>'
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/animals')
 def animals():
     animal_db = get_animal_db()
     return render_template('animals.html', animals=animal_db)
 
+
 @app.route('/quiz')
 def quiz():
     return render_template("quiz.html")
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            # return log_the_user_in(request.form['username'])
-            return request.form['username']
-        else:
-            error = 'Invalid username/password'
-    return render_template('login.html', error=error)
+@app.route('/todo')
+def todo():
+    todo_list = Todo.query.all()
+    return render_template('todo.html', todo_list=todo_list)
+
+
+@app.route('/todo/add', methods=['POST'])
+def todo_add():
+    title = request.form.get("title")
+    if title != "":
+        new_todo = Todo(title=title, complete=False, visible=True)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for("todo"))
+
+
+@app.route('/todo/check/<int:todo_id>')
+def todo_check(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("todo"))
+
+
+@app.route('/todo/hide/<int:todo_id>')
+def todo_hide(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.visible = False
+    db.session.commit()
+    return redirect(url_for("todo"))
 
 
 def get_animal_db():
@@ -49,3 +84,8 @@ def get_animal_db():
                 }
             animal_db.append(current_animal)
     return animal_db
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(host='0.0.0.0')
