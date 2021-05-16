94d30920964272c81a9237e1f815d97285998293
joearrowsmith <joearrowsmith98@gmail.com>
2021-05-16 17:26:45 +0100

    public

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def get_animal_db():
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(host='localhost', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=5000)
